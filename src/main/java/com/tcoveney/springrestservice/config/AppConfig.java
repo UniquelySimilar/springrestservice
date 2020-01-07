85cb9a78df2034cde462ef02e1be8f27fa3ee7e8
UniquelySimilar <consciousexplorer@gmail.com>
2020-01-07 15:38:58 -0700

    resolved error from jackson trying to serialize uninitialized (lazy loaded) child Order collection of Customer object

--- src/main/java/com/tcoveney/springrestservice/config/AppConfig.java
+++ src/main/java/com/tcoveney/springrestservice/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.tcoveney.springrestservice.config;
 
+import java.util.List;
 import java.util.Properties;
 
 import javax.naming.NamingException;
@@ -11,17 +12,24 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.jndi.JndiTemplate;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 
 @Configuration
 @EnableTransactionManagement
 @EnableWebMvc
 @ComponentScan(basePackages = "com.tcoveney")
-public class AppConfig {
+public class AppConfig extends WebMvcConfigurerAdapter {
 	@Bean(destroyMethod="")
 	DataSource dataSource() throws NamingException {
 		JndiTemplate jndiTemplate = new JndiTemplate();
@@ -53,5 +61,26 @@ public class AppConfig {
 	    messageSource.setDefaultEncoding("UTF-8");
 	    return messageSource;
 	}
+	
+    /* Here we register the Hibernate5Module into an ObjectMapper, then set this custom-configured ObjectMapper
+     * to the MessageConverter and return it to be added to the HttpMessageConverters of our application*/
+    public MappingJackson2HttpMessageConverter jacksonMessageConverter(){
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+
+        ObjectMapper mapper = new ObjectMapper();
+        //Registering Hibernate5Module to support lazy objects
+        mapper.registerModule(new Hibernate5Module());
+
+        messageConverter.setObjectMapper(mapper);
+        return messageConverter;
+
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        //Here we add our custom-configured HttpMessageConverter
+        converters.add(jacksonMessageConverter());
+        super.configureMessageConverters(converters);
+    }
 
 }
